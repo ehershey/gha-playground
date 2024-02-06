50c211f6d950bb07eeaefa29235622ea3a21cf6d
Ernie Hershey <github@ernie.org>
2024-02-06 15:48:21 -0500

    python test file

---
+++ tests.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Test random stuff
+"""
+
+def test_one_thing():
+    """
+    Test a thing
+    """
+
+    for date in ['2020-10-22', '2020-10-19', '2020-07-29']:
+        assert date is not None
+
+def test_another_thing():
+    """
+    Test another thing
+    """
+    assert "ernie" != "hershey"
